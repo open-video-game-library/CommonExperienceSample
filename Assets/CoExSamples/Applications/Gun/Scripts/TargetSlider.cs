9c11964358d8d6d16ce6883f2ff6decc738c53ee
tkystar <tky.star.1231@gmail.com>
2022-07-19 16:33:40 +0900

    “fix:ターゲットモデルを差し替え”

--- Assets/CoExSamples/Applications/Gun/Scripts/TargetSlider.cs
+++ Assets/CoExSamples/Applications/Gun/Scripts/TargetSlider.cs
@@ -6,11 +6,13 @@ namespace GunGame
 {
     public class TargetSlider : MonoBehaviour
     {
-        [SerializeField] private float _width;
+        [SerializeField] private float _width = 1.0f;
 
-        [SerializeField] private float _speed;
+        [SerializeField] private float _speed = 0.5f;
 
         private Vector3 _startPosition;
+
+        [SerializeField] private bool _static;
 
         // Start is called before the first frame update
         void Start()
@@ -21,7 +23,11 @@ namespace GunGame
         // Update is called once per frame
         void Update()
         {
-            SlideTarget();
+            if (!_static)
+            {
+                SlideTarget();
+            }
+            
         }
 
         private void SlideTarget()
